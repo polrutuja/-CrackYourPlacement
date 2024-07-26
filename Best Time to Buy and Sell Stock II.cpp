56da7bccdb266971a172c620840a3e1832b98a80
polrutuja <162364185+polrutuja@users.noreply.github.com>
2024-07-26 10:31:15 +0530

    Create Best Time to Buy and Sell Stock II.cpp

---
+++ Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+//TC=O(N) SC=O(1)
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i] > prices[i-1]){
+                profit +=  prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+};
